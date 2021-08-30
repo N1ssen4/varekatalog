41ba7fbdeadd9249b45d4896d829dc772b745464
Mads <mads.rosenlund@hotmail.com>
2021-08-30 15:35:23 +0200

    Initial commit

---
+++ src/test/java/com/kea/demo/VarekatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarekatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
